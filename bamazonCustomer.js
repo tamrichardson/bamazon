012f3a95d6c9a2fb556c8940cf562350821263e8
Tammy Richardson <atrich0220@gmail.com>
2020-04-22 12:15:17 -0600

    added update function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,6 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-
     //console.log("connected as id " + connection.threadId);
     afterConnection();
 });
@@ -21,13 +20,11 @@ console.log(chalk.green("WELCOME TO THE BAMAZON SUPERSTORE"));
 console.log(chalk.magenta("\n************************************\n"));
 
 function afterConnection() {
-
     inquirer.prompt({
         type: "list",
         name: "userOptions",
         message: "What would you like to do?",
         choices: ["View products", "Make a purchase", "Sell a product", "Exit"]
-
     }).then(function (response) {
         console.log(response.userOptions)
         if (response.userOptions === "View products") {
@@ -40,87 +37,104 @@ function afterConnection() {
         } if (response.userOptions === "Make a purchase") {
             console.table(response);
             choose();
-            //chooseQuantity()
         } if (response.userOptions === "Sell a product") {
             sell();
         } if (response.userOptions === "Exit") {
             exit();
         }
     })
-}
 
-function choose() {
-    connection.query('SELECT item_id, product_name FROM productsTable', function (err, response) {
-        // console.table(response);
-        // console.log(typeof (response))
-        var idArray = [];
-        for (var i = 0; i < response.length; i++) {
-            // console.log(response[i].item_id);
-            idArray.push(response[i].item_id);
-        }
-        //console.log(idArray);
-        inquirer
-            .prompt([
-                {
-                    type: 'list',
-                    name: 'selection',
-                    message: 'Choose the ID of the product you would like to buy.',
-                    choices: idArray,
-                },
-                {
-                    type: 'input',
-                    name: 'quantity',
-                    message: 'How many would you would like to buy?',
-                },
-            ])
-            .then(function (response) {
-                console.log('You chose ID#: ' + response.selection + ' and you want: ' + response.quantity + '.');
-            });
-    });
-}
-
-function sell() {
-    connection.query('SELECT item_id, product_name FROM productsTable', function (err, response) {
-        // console.table(response);
-        // console.log(typeof (response))
-        var idArray = [];
-        for (var i = 0; i < response.length; i++) {
-            // console.log(response[i].item_id);
-            idArray.push(response[i].item_id);
-        }
-        //console.log(idArray);
-        inquirer
-            .prompt([
-                {
-                    type: 'list',
-                    name: 'selection',
-                    message: 'Choose the ID of the product you would like to sell.',
-                    choices: idArray,
-                },
-                {
-                    type: 'input',
-                    name: 'quantity',
-                    message: 'How many would you would like to sell?',
-                },
-            ])
-            .then(function (response) {
-                console.log('You chose ID#: ' + response.selection + ' and you want sell: ' + response.quantity + '.');
-            });
-    });
-}
-
-function exit() {
-    inquirer
-        .prompt({
-            type: "confirm",
-            name: "quit",
-            message: "Would you like to exit Bamazon?",
-        }
-        ).then(function (response) {
-            if (response.quit === false) {
-                afterConnection();
-            } else {
-                connection.end();
+    function choose() {
+        connection.query('SELECT item_id, product_name FROM productsTable', function (err, response) {
+            // console.table(response);
+            // console.log(typeof (response))
+            var idArray = [];
+            for (var i = 0; i < response.length; i++) {
+                // console.log(response[i].item_id);
+                idArray.push(response[i].item_id);
             }
-        })
-}
+            //console.log(idArray);
+            inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        name: 'selection',
+                        message: 'Choose the ID of the product you would like to buy.',
+                        choices: idArray,
+                    },
+                    {
+                        type: 'input',
+                        name: 'quantity',
+                        message: 'How many would you would like to buy?',
+                    },
+                ])
+                .then(function (response) {
+                    console.log('You chose ID#: ' + response.selection + ' and you want: ' + response.quantity + '.');
+                    var query = "SELECT * from productsTable WHERE item_id=?";
+                    connection.query(query, [response.selection], function (err, res) {
+                        if (err)
+                            throw err;
+                        if (response.quantity > response.stock_quantity) {
+                            console.log("Sorry! Only " + response.stock_quantity + " in stock.")
+                        } else {
+                            var sales = response.price * response.quantity;
+                            console.log("Your total is: $" + sales);
+                            var newQuantity = response.stock_quantity - parseInt(response.quantity);
+                            connection.query("UPDATE quantity SET ? WHERE ?", [
+                                {
+                                    stock_quantity: newQuantity
+                                },
+                                {
+                                    item_id: response.selection
+                                }
+                            ],
+
+                            )
+                        }
+                    });
+
+                    function sell() {
+                        connection.query('SELECT item_id, product_name FROM productsTable', function (err, response) {
+                            // console.table(response);
+                            // console.log(typeof (response))
+                            var idArray = [];
+                            for (var i = 0; i < response.length; i++) {
+                                // console.log(response[i].item_id);
+                                idArray.push(response[i].item_id);
+                            }
+                            //console.log(idArray);
+                            inquirer
+                                .prompt([
+                                    {
+                                        type: 'list',
+                                        name: 'selection',
+                                        message: 'Choose the ID of the product you would like to sell.',
+                                        choices: idArray,
+                                    },
+                                    {
+                                        type: 'input',
+                                        name: 'quantity',
+                                        message: 'How many would you would like to sell?',
+                                    },
+                                ])
+                                .then(function (response) {
+                                    console.log('You chose ID#: ' + response.selection + ' and you want to sell: ' + response.quantity + '.');
+
+                                });
+                        })
+                        function exit() {
+                            inquirer
+                                .prompt({
+                                    type: "confirm",
+                                    name: "quit",
+                                    message: "Would you like to exit Bamazon?",
+                                }
+                                ).then(function (response) {
+                                    if (response.quit === false) {
+                                        afterConnection();
+                                    } else {
+                                        connection.end();
+                                    }
+                                })
+                        }
+
